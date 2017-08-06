8fc67c042008713ec74b39d0131ad02191fca85c
Muhammad Qureshi <muhammadqureshi@Muhammads-MacBook-Pro.local>
2017-08-06 20:26:36 +0100

    first commit

---
+++ src/EarthquakeCityMap.java
@@ -0,0 +1,22 @@
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import processing.core.PApplet;
+
+public class EarthquakeCityMap extends PApplet {
+    private UnfoldingMap map;
+
+    public void setup() {
+        size(950, 600, OPENGL);
+        map = new UnfoldingMap(this, 200, 50, 700, 500,
+                new Google.GoogleMapProvider());
+        map.zoomToLevel(2);
+        MapUtils.createDefaultEventDispatcher(this, map);
+    }
+
+    public void draw(){
+        background(10);
+        map.draw();
+//        addKey();
+    }
+}
---
+++ src/HelloUnfoldingWorld.java
@@ -0,0 +1,31 @@
+import processing.core.PApplet;
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import de.fhpotsdam.unfolding.providers.OpenStreetMap.*;
+
+/**
+ * Hello Unfolding World.
+ * 
+ * Download the distribution with examples for many more examples and features.
+ */
+public class HelloUnfoldingWorld extends PApplet {
+
+	UnfoldingMap map;
+
+	public void setup() {
+		size(800, 600, OPENGL);
+
+//		map = new UnfoldingMap(this);
+		map = new UnfoldingMap(this, new OpenStreetMapProvider());
+		map.zoomAndPanTo(10, new Location(52.5f, 13.4f));
+
+		MapUtils.createDefaultEventDispatcher(this, map);
+	}
+
+	public void draw() {
+		background(0);
+		map.draw();
+	}
+
+}
